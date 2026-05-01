dc42259e59f0e5e3ecd575ea6bdaa9d136183231
Francisco Rafael P. Rodrigues <franciscocjn06@gmail.com>
2026-05-01 14:44:56 -0300

    feat: criando projeto

---
+++ src/backend/gamesocket/src/main/java/com/example/gamesocket/GamesocketApplication.java
@@ -0,0 +1,13 @@
+package com.example.gamesocket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GamesocketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GamesocketApplication.class, args);
+	}
+
+}
---
+++ src/backend/gamesocket/src/test/java/com/example/gamesocket/GamesocketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gamesocket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GamesocketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
